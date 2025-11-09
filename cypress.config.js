707b550a4d6561ff954b4be27a609872d651ddf4
Vinicios Neves <vinicios.neves@mac>
2025-11-09 16:04:49 +0000

    video 4.3

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
     e2e: {
         baseUrl: 'http://localhost:5173',
+        screenshotOnRunFailure: true,
+        screenshotsFolder: 'cypress/screenshots',
+        video: false,
         setupNodeEvents() {
             // implement node event listeners here
         },
--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -42,6 +42,8 @@ describe('Navegação do Feed para Post', () => {
 
         // Verificar que o feed está visível com posts usando texto ao invés de h2
         cy.contains('Introdução ao React', { timeout: 10000 }).should('exist')
+        
+        cy.contains('Falha proposital', { timeout: 10000 }).should('exist')
 
         // Encontrar o link "Ver detalhes" associado ao post "Introdução ao React"
         // Usando uma estratégia mais robusta: encontrar o texto do título e depois o link próximo
